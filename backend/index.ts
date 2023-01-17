26b31f277556c001a0e5937a9b9a38ee989528f6
LinuzJ <linusjern00@gmail.com>
2023-01-17 18:18:08 +0200

    Tabs to spaces

--- backend/index.ts
+++ backend/index.ts
@@ -3,24 +3,24 @@ import { XMLParser } from "fast-xml-parser"
 import cors from "cors"
 
 interface Drone {
-	serialNumber: string
-	positionY: number
-	positionX: number
+  serialNumber: string
+  positionY: number
+  positionX: number
 }
 
 interface Pilot {
-	pilotId: string
-	firstName: string
-	lastName: string
-	phoneNumber: string
-	createdDt: string
-	email: string
+  pilotId: string
+  firstName: string
+  lastName: string
+  phoneNumber: string
+  createdDt: string
+  email: string
 }
 
 type DroneObservation = Pilot & {
-	updatedAt: Date
-	closestDistance: number
-	mostRecentDistance: number
+  updatedAt: Date
+  closestDistance: number
+  mostRecentDistance: number
 }
 
 const app = express()
@@ -28,19 +28,19 @@ app.use(cors())
 const port = 5001
 
 app.get("/status", (_, res) => {
-	const statusResponse = Object.keys(observations).map((serialNumber) => {
-		return {
-			...observations[serialNumber],
-			serialNumber: serialNumber,
-		}
-	})
-	statusResponse.sort((a, b) => a.mostRecentDistance - b.mostRecentDistance)
+  const statusResponse = Object.keys(observations).map((serialNumber) => {
+    return {
+      ...observations[serialNumber],
+      serialNumber: serialNumber,
+    }
+  })
+  statusResponse.sort((a, b) => a.mostRecentDistance - b.mostRecentDistance)
 
-	res.send(statusResponse)
+  res.send(statusResponse)
 })
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
 
 const dronesUrl: string = "assignments.reaktor.com/birdnest/drones"
@@ -50,99 +50,99 @@ const pilotUrl: string = "assignments.reaktor.com/birdnest/pilots/"
 const observations: { [serialNumber: string]: DroneObservation } = {}
 
 setInterval(async () => {
-	updatePilots()
+  updatePilots()
 
-	// Remove old pilots
-	const currentTime = new Date()
-	for (var key of Object.keys(observations)) {
-		if (
-			currentTime.getTime() - observations[key].updatedAt.getTime() >
-			10 * 60 * 1000
-		) {
-			delete observations[key]
-		}
-	}
+  // Remove old pilots
+  const currentTime = new Date()
+  for (var key of Object.keys(observations)) {
+    if (
+      currentTime.getTime() - observations[key].updatedAt.getTime() >
+      10 * 60 * 1000
+    ) {
+      delete observations[key]
+    }
+  }
 }, 2000)
 
 async function updatePilots() {
-	const drones = await getAllCurrentDrones()
-	const dronesWithinRange = drones.filter((drone) => {
-		return distanceBetweenDroneAndCenter(drone) < 100000
-	})
+  const drones = await getAllCurrentDrones()
+  const dronesWithinRange = drones.filter((drone) => {
+    return distanceBetweenDroneAndCenter(drone) < 100000
+  })
 
-	const currentTime: Date = new Date()
-	for (var drone of dronesWithinRange) {
-		const distance = distanceBetweenDroneAndCenter(drone)
+  const currentTime: Date = new Date()
+  for (var drone of dronesWithinRange) {
+    const distance = distanceBetweenDroneAndCenter(drone)
 
-		if (drone.serialNumber in observations) {
-			observations[drone.serialNumber].updatedAt = currentTime
+    if (drone.serialNumber in observations) {
+      observations[drone.serialNumber].updatedAt = currentTime
 
-			observations[drone.serialNumber].mostRecentDistance =
-				convertToMeters(distance)
-			if (
-				convertToMeters(distance) <
-				observations[drone.serialNumber].closestDistance
-			) {
-				observations[drone.serialNumber].closestDistance =
-					convertToMeters(distance)
-			}
-		} else {
-			const pilot = await getPilotInfo(drone.serialNumber)
+      observations[drone.serialNumber].mostRecentDistance =
+        convertToMeters(distance)
+      if (
+        convertToMeters(distance) <
+        observations[drone.serialNumber].closestDistance
+      ) {
+        observations[drone.serialNumber].closestDistance =
+          convertToMeters(distance)
+      }
+    } else {
+      const pilot = await getPilotInfo(drone.serialNumber)
 
-			observations[drone.serialNumber] = {
-				...pilot,
-				updatedAt: currentTime,
-				closestDistance: convertToMeters(distance),
-				mostRecentDistance: convertToMeters(distance),
-			}
-		}
-	}
+      observations[drone.serialNumber] = {
+        ...pilot,
+        updatedAt: currentTime,
+        closestDistance: convertToMeters(distance),
+        mostRecentDistance: convertToMeters(distance),
+      }
+    }
+  }
 }
 
 // Function to get all drones within 500m of the birdsnest
 async function getAllCurrentDrones(): Promise<Drone[]> {
-	const parser = new XMLParser()
+  const parser = new XMLParser()
 
-	// request from api
-	const response = await fetch(dronesUrl)
-	const xmlAsText = await response.text()
+  // request from api
+  const response = await fetch(dronesUrl)
+  const xmlAsText = await response.text()
 
-	const parsed = await parser.parse(xmlAsText)
-	// We could check that the type is correct but for the purpose of this
-	// exercise we simply cast it.
-	const drones: Drone[] = parsed.report.capture.drone
+  const parsed = await parser.parse(xmlAsText)
+  // We could check that the type is correct but for the purpose of this
+  // exercise we simply cast it.
+  const drones: Drone[] = parsed.report.capture.drone
 
-	return drones
+  return drones
 }
 
 async function getPilotInfo(serialNumber: string): Promise<Pilot> {
-	const response = await fetch(`${pilotUrl}${serialNumber}`)
+  const response = await fetch(`${pilotUrl}${serialNumber}`)
 
-	// Again, we assume the response type is correct.
-	return await response.json()
+  // Again, we assume the response type is correct.
+  return await response.json()
 }
 
 interface Coord {
-	x: number
-	y: number
+  x: number
+  y: number
 }
 
 function distanceBetweenDroneAndCenter(drone: Drone): number {
-	const centerCoords = {
-		x: 250000,
-		y: 250000,
-	}
-	const droneCoords = {
-		x: drone.positionX,
-		y: drone.positionY,
-	}
-	return distance(centerCoords, droneCoords)
+  const centerCoords = {
+    x: 250000,
+    y: 250000,
+  }
+  const droneCoords = {
+    x: drone.positionX,
+    y: drone.positionY,
+  }
+  return distance(centerCoords, droneCoords)
 }
 
 function distance(a: Coord, b: Coord): number {
-	return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2))
+  return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2))
 }
 
 function convertToMeters(x: number): number {
-	return Math.round(x / 100) / 10
+  return Math.round(x / 100) / 10
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,12 +3,12 @@ import { Table } from "rsuite"
 import "rsuite-table/dist/css/rsuite-table.css"
 
 interface Pilot {
-	pilotId: string
-	firstName: string
-	lastName: string
-	phoneNumber: string
-	createdDt: string
-	email: string
+  pilotId: string
+  firstName: string
+  lastName: string
+  phoneNumber: string
+  createdDt: string
+  email: string
 }
 
 type Status = Pilot & { updatedAt: Date; serialNumber: string }
@@ -19,97 +19,97 @@ const backendUrl = process.env.NODE_ENV === "production" ? "" : localUrl
 const { Column, HeaderCell, Cell } = Table
 
 export default function Birdnest({
-	initialStatus,
+  initialStatus,
 }: {
-	initialStatus: Status[]
+  initialStatus: Status[]
 }) {
-	const [rows, setRows] = useState<Status[]>(initialStatus)
+  const [rows, setRows] = useState<Status[]>(initialStatus)
 
-	useEffect(() => {
-		let interval = setInterval(async () => {
-			const res = await fetch(`${backendUrl}/status`, {
-				method: "GET",
-			})
-			const data = await res.json()
-			setRows(data)
-		}, 2000)
+  useEffect(() => {
+    let interval = setInterval(async () => {
+      const res = await fetch(`${backendUrl}/status`, {
+        method: "GET",
+      })
+      const data = await res.json()
+      setRows(data)
+    }, 2000)
 
-		return () => {
-			clearInterval(interval)
-		}
-	}, [rows])
+    return () => {
+      clearInterval(interval)
+    }
+  }, [rows])
 
-	return (
-		<div
-			style={{
-				display: "flex",
-				justifyContent: "space-evenly",
-				flexDirection: "column",
-				fontFamily: "monospace",
-				alignItems: "center",
-				padding: "0 10%",
-			}}
-		>
-			<p style={{ fontSize: "3rem", color: "navy" }}>Birdnest</p>
-			<p style={{ padding: "0 20px" }}>
-				List of all pilots that have flown their drones within 100m of the
-				birdnest in the past 10min
-			</p>
-			<div style={{ width: "100%", maxWidth: "1200px" }}>
-				<Table height={500} data={rows} wordWrap='break-word'>
-					<Column width={150} align='center' fixed>
-						<HeaderCell style={{ color: "navy" }}>Pilot ID</HeaderCell>
-						<Cell dataKey='pilotId' />
-					</Column>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexDirection: "column",
+        fontFamily: "monospace",
+        alignItems: "center",
+        padding: "0 10%",
+      }}
+    >
+      <p style={{ fontSize: "3rem", color: "navy" }}>Birdnest</p>
+      <p style={{ padding: "0 20px" }}>
+        List of all pilots that have flown their drones within 100m of the
+        birdnest in the past 10min
+      </p>
+      <div style={{ width: "100%", maxWidth: "1200px" }}>
+        <Table height={500} data={rows} wordWrap='break-word'>
+          <Column width={150} align='center' fixed>
+            <HeaderCell style={{ color: "navy" }}>Pilot ID</HeaderCell>
+            <Cell dataKey='pilotId' />
+          </Column>
 
-					<Column width={100}>
-						<HeaderCell style={{ color: "navy" }}>First Name</HeaderCell>
-						<Cell dataKey='firstName' />
-					</Column>
+          <Column width={100}>
+            <HeaderCell style={{ color: "navy" }}>First Name</HeaderCell>
+            <Cell dataKey='firstName' />
+          </Column>
 
-					<Column width={100}>
-						<HeaderCell style={{ color: "navy" }}>Last Name</HeaderCell>
-						<Cell dataKey='lastName' />
-					</Column>
+          <Column width={100}>
+            <HeaderCell style={{ color: "navy" }}>Last Name</HeaderCell>
+            <Cell dataKey='lastName' />
+          </Column>
 
-					<Column width={140}>
-						<HeaderCell style={{ color: "navy" }}>Phone Number</HeaderCell>
-						<Cell dataKey='phoneNumber' />
-					</Column>
+          <Column width={140}>
+            <HeaderCell style={{ color: "navy" }}>Phone Number</HeaderCell>
+            <Cell dataKey='phoneNumber' />
+          </Column>
 
-					<Column width={250}>
-						<HeaderCell style={{ color: "navy" }}>Email</HeaderCell>
-						<Cell dataKey='email' />
-					</Column>
+          <Column width={250}>
+            <HeaderCell style={{ color: "navy" }}>Email</HeaderCell>
+            <Cell dataKey='email' />
+          </Column>
 
-					<Column width={170}>
-						<HeaderCell style={{ color: "navy" }}>
-							Drone Serial Number
-						</HeaderCell>
-						<Cell dataKey='serialNumber' />
-					</Column>
-					<Column width={80}>
-						<HeaderCell style={{ color: "navy" }}>
-							Most Recent Distance (m)
-						</HeaderCell>
-						<Cell dataKey='mostRecentDistance' />
-					</Column>
-					<Column width={80}>
-						<HeaderCell style={{ color: "navy" }}>
-							Closest Distance (m)
-						</HeaderCell>
-						<Cell dataKey='closestDistance' />
-					</Column>
-				</Table>
-			</div>
-		</div>
-	)
+          <Column width={170}>
+            <HeaderCell style={{ color: "navy" }}>
+              Drone Serial Number
+            </HeaderCell>
+            <Cell dataKey='serialNumber' />
+          </Column>
+          <Column width={80}>
+            <HeaderCell style={{ color: "navy" }}>
+              Most Recent Distance (m)
+            </HeaderCell>
+            <Cell dataKey='mostRecentDistance' />
+          </Column>
+          <Column width={80}>
+            <HeaderCell style={{ color: "navy" }}>
+              Closest Distance (m)
+            </HeaderCell>
+            <Cell dataKey='closestDistance' />
+          </Column>
+        </Table>
+      </div>
+    </div>
+  )
 }
 
 export async function getServerSideProps() {
-	const res = await fetch(`${localUrl}/status`)
-	const initialStatus = await res.json()
+  const res = await fetch(`${localUrl}/status`)
+  const initialStatus = await res.json()
 
-	// Pass data to the page via props
-	return { props: { initialStatus } }
+  // Pass data to the page via props
+  return { props: { initialStatus } }
 }
